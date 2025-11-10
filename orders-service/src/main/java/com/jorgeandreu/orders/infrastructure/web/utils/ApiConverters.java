2bdd2e7b095ef49ef31d53114aa91f33af9bae2a
Zephyrioth <45359089+Zephyrioth@users.noreply.github.com>
2025-11-10 19:27:19 +0100

    Changes pass

--- orders-service/src/main/java/com/jorgeandreu/orders/infrastructure/web/utils/ApiConverters.java
+++ orders-service/src/main/java/com/jorgeandreu/orders/infrastructure/web/utils/ApiConverters.java
@@ -19,7 +19,7 @@ public interface ApiConverters {
     default Double map(BigDecimal v) { return v == null ? null : v.doubleValue(); }
 
     default <T> Optional<T> map(T v) { return Optional.ofNullable(v); }
-    default <T> T map(Optional<T> v) { return v.orElse(null); }
+    default <T> T map(Optional<T> v) { return v == null ? null : v.orElse(null); }
 
     default OrderStatus map(Order.StatusEnum v) { return v == null ? null : OrderStatus.valueOf(v.getValue()); }
     default Order.StatusEnum map(OrderStatus v) { return v == null ? null : Order.StatusEnum.fromValue(v.name()); }
